c0a8226e91d059b873e1e65786715f07ad949609
Arnob Chowdhury <chowdhury.sami.al@gmail.com>
2022-08-30 21:46:27 +0600

    Delete repeated claims with script

--- src/issuer_id.ts
+++ src/issuer_id.ts
@@ -1,21 +1,30 @@
 import "dotenv/config";
 import { prisma } from "./db/prisma";
-import { Claim } from "prisma/prisma-client";
-import { writeFile } from "fs";
-import path from "path";
 
 const updateIssuerIdScript = async () => {
   console.log("Fetching all data");
+  const acc: { [key: string]: any } = {};
+  const repeatedIds: any[] = [];
   const allClaims = await prisma.claim.findMany({});
 
-  const asJson = JSON.stringify(allClaims);
-  const filePath = path.resolve(__dirname, "..", "alldata.json");
-
-  writeFile(filePath, asJson, (err) => {
-    if (err) {
-      console.log(err);
+  allClaims.forEach((claim) => {
+    if (claim.object) {
+      if (acc[claim.object]) {
+        repeatedIds.push(claim.id);
+      } else {
+        acc[claim.object] = true;
+      }
     }
   });
+
+  const { count } = await prisma.claim.deleteMany({
+    where: {
+      id: {
+        in: repeatedIds,
+      },
+    },
+  });
+  console.log(`${count} claim/s deleted.`);
 
   return;
 };
