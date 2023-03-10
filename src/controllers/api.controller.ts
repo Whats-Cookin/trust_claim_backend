d47dbafc70bb3149f4e4ce909bddb403dff39829
Golda Velez <golda@3box.io>
2023-03-09 17:05:39 -0700

    changes to models

--- src/controllers/api.controller.ts
+++ src/controllers/api.controller.ts
@@ -4,28 +4,6 @@ import { Prisma } from "prisma/prisma-client";
 import { passToExpressErrorHandler, turnFalsyPropsToUndefined } from "../utils";
 import createError from "http-errors";
 import axios from "axios";
-import { z, AnyZodObject, ZodError } from "zod";
-
-export async function zParse<T extends AnyZodObject>(
-  schema: T,
-  req: Request
-): Promise<z.infer<T>> {
-  return schema.parseAsync(req);
-}
-
-const simpleClaimSchema = z.object({
-  body: z.object({
-    subject: z.string(),
-    object: z.string(),
-    claim: z.string(),
-    statement: z.string().optional(),
-    aspect: z.string().optional(),
-    howKnown: z.string().optional(),
-    source: z.string().optional(),
-    effectiveDate: z.string().optional(),
-    confidence: z.number().optional(),
-  }),
-});
 
 export const claimPost = async (
   req: Request,
@@ -33,16 +11,14 @@ export const claimPost = async (
   next: NextFunction
 ) => {
   let claim;
-  const { body } = simpleClaimSchema.parse(req)
   try {
     const userId = (req as ModifiedRequest).userId;
-    const sourceURI = body.source;
+    const rawClaim: any = turnFalsyPropsToUndefined(req.body);
     claim = await prisma.claim.create({
       data: {
         issuerId: `http://trustclaims.whatscookin.us/users/${userId}`,
         issuerIdType: "URL",
-        sourceURI,
-        ...body,
+        ...rawClaim,
       },
     });
   } catch (err) {
