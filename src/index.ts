29032988e14bb3b91c4b8acc9e60f5f6b6033a89
Ahmed Hany <dev.ahmedhany@gmail.com>
2023-04-12 01:28:33 +0200

    cors live.linkedtrust.us

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://live.linkedtrust.us']
+}));
 
 app.use("/auth", authRoutes);
 app.use("/api", apiRoutes);
