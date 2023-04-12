a140ea69233f93fcfceb948b069b1871a0340a86
AhmedAbdelmenam <ahmedabdelmenam534@gmail.com>
2023-04-12 14:51:22 +0200

    Merge pull request #49 from Whats-Cookin/Ahmed-paganation-of-node-endpoint
    paganation of node endpoint

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use("/auth", authRoutes);
 app.use("/api", apiRoutes);
