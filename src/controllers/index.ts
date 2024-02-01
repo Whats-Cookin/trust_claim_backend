49bd47057f1e33017e6e90830f424f67a94e1960
0marSalah <omar.salah1597@gmail.com>
2024-02-01 22:30:14 +0200

    fix search router

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,7 +1,16 @@
-export { claimPost, claimGet, claimsGet, claimsFeed, nodesGet } from "./api.controller";
+export {
+  claimPost,
+  claimsGet,
+  claimsFeed,
+  nodesGet,
+  claimGetById,
+  claimSearch,
+  claimReport,
+  getNodeForLoggedInUser
+} from "./api.controller";
 export {
   login,
   signup,
   refreshToken,
-  githubAuthenticator,
+  githubAuthenticator
 } from "./auth.controller";
