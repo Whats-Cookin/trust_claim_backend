593079bb8e34285c05e73b4a46eb2482cb34d678
Omar Eloui <contact@omareloui.com>
2024-10-03 18:35:08 +0300

    fix(docs): fix the swagger docs path

--- src/swagger/index.ts
+++ src/swagger/index.ts
@@ -5,7 +5,7 @@ import { type Router } from "express";
 import swaggerUi from "swagger-ui-express";
 
 export function registerSwagger(router: Router) {
-  const yamlDoc = readFileSync("./docs/swagger/swagger.yaml", { encoding: "utf-8" });
+  const yamlDoc = readFileSync("../docs/swagger/swagger.yaml", { encoding: "utf-8" });
   const swaggerDocument = yaml.parse(yamlDoc);
 
   router.use("/docs", swaggerUi.serve);
