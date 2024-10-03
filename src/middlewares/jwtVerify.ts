e509e7d6a2bd64c3e31905f9c3c26aea646a3e35
Omar Eloui <contact@omareloui.com>
2024-10-03 11:56:11 +0300

    fix(auth): throw error if the user isn't authenticated

--- src/middlewares/jwtVerify.ts
+++ src/middlewares/jwtVerify.ts
@@ -2,26 +2,20 @@ import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import createError from "http-errors";
 
-export const jwtVerify = (req: Request, _res: Response, next: NextFunction) => {
-  const token = req.headers.authorization?.split(" ")[1];
-  if (token) {
-    try {
-      const decoded = jwt.verify(token, process.env.ACCESS_SECRET as string);
-      (req as ModifiedRequest).isAuthenticated = true;
-      (req as ModifiedRequest).userId = parseInt((decoded as JWTDecoded).aud);
-      next();
-    } catch (err: any) {
-      const message =
-        err.name === "JsonWebTokenError" ? "Unauthorized" : err.message;
-      next(new createError.Unauthorized(message));
+export function jwtVerify(req: Request, _res: Response, next: NextFunction) {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+      throw new Error("Unauthorized");
     }
-  } else {
-    (req as ModifiedRequest).isAuthenticated = false;
-    (req as ModifiedRequest).userId = 0;
-    // now we will need to check the did is right
-    // we can verify the request directly if it has a claimAddress
-    // so we don't really need a user login
-    //    next(new createError.Unauthorized("Access token required"));
+
+    const decoded = jwt.verify(token, process.env.ACCESS_SECRET as string);
+    (req as ModifiedRequest).isAuthenticated = true;
+    (req as ModifiedRequest).userId = parseInt((decoded as JWTDecoded).aud);
     next();
+  } catch (err: any) {
+    const message = err.name === "JsonWebTokenError" ? "Unauthorized" : err.message;
+    next(new createError.Unauthorized(message));
   }
-};
+}
