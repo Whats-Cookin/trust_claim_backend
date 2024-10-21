ed4a2124fd2458689cb2d8175f8db2255d7ad2f5
AhmedAbdelmenam <ahmedabdelmenam534@gmail.com>
2024-10-21 16:15:18 +0300

    change swagger path

--- src/swagger/index.ts
+++ src/swagger/index.ts
@@ -1,16 +1,17 @@
 import { readFileSync } from "node:fs";
-
+import * as path from 'path';
 import yaml from "yaml";
 import { type Router } from "express";
 import swaggerUi from "swagger-ui-express";
 
 export function registerSwagger(router: Router) {
-  const yamlDoc = readFileSync(
-    process.env.NODE_ENV === "development" ? "./docs/swagger/swagger.yaml" : "../docs/swagger/swagger.yaml",
-    { encoding: "utf-8" },
-  );
-  const swaggerDocument = yaml.parse(yamlDoc);
-
-  router.use("/docs", swaggerUi.serve);
-  router.get("/docs", swaggerUi.setup(swaggerDocument));
+  const yamlPath = path.join(__dirname, '../../docs/swagger/swagger.yaml');
+  try {
+    const yamlDoc = readFileSync(yamlPath, { encoding: "utf-8" });
+    const swaggerDocument = yaml.parse(yamlDoc);
+    router.use("/docs", swaggerUi.serve);
+    router.get("/docs", swaggerUi.setup(swaggerDocument));
+  } catch (error) {
+    console.error("Error loading Swagger YAML file:", error);
+  }
 }
