d599c46ff4f746e93c807c4f21b2c95365979caa
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2025-03-13 02:06:58 +0200

    refactor(graph): restructure GraphNode and GraphEdge interfaces to use nested data objects and enhance getGraphNode function for improved clarity

--- src/dao/graph.ts
+++ src/dao/graph.ts
@@ -1,21 +1,24 @@
 import { Prisma } from "@prisma/client";
 import { prisma } from "../db/prisma";
-import { makeClaimSubjectURL } from "../utils";
+import { getClaimNameFromNodeUri, makeClaimSubjectURL } from "../utils";
 
 interface GraphNode {
-  id: string;
-  label: string;
-  image: string;
-  raw: any;
+  data: {
+    id: string;
+    label: string;
+    raw: any;
+  };
 }
 
 interface GraphEdge {
-  id: string;
-  relation: string;
-  source: string;
-  target: string;
-  label: string;
-  raw: any;
+  data: {
+    id: string;
+    relation: string;
+    source: string;
+    target: string;
+    label: string;
+    raw: any;
+  };
 }
 
 interface GraphResponse {
@@ -23,9 +26,8 @@ interface GraphResponse {
   edges: GraphEdge[];
 }
 
-export const getGraphNode = async (claimId: string | number, page: number, limit: number, host: string) => {
-  try {
-    const baseQuery = `
+export const getGraphNode = async (claimId: string | number, page: number, limit: number, host: string): Promise<GraphResponse> => {
+  const baseQuery = `
     SELECT DISTINCT ON (c.id)
     c.id AS id,
     c.subject AS label,
@@ -39,91 +41,81 @@ export const getGraphNode = async (claimId: string | number, page: number, limit
     JOIN "Node" AS n1 ON e."startNodeId" = n1.id
     JOIN "Node" AS n2 ON e."endNodeId" = n2.id
    `;
-    const claim_as_node_uri = makeClaimSubjectURL(claimId.toString(), host);
+  const claim_as_node_uri = makeClaimSubjectURL(claimId.toString(), process.env.BASE_URL || host);
 
-    let validations = await prisma.$queryRaw<any[]>`
-          ${Prisma.raw(baseQuery)}
-          WHERE n1."nodeUri" = ${claim_as_node_uri} AND c."id" != ${Number(claimId)}
-          ORDER BY c.id DESC
-          LIMIT ${limit}
-          OFFSET ${(page - 1) * limit}
-        `;
-
-    let claimNode = await prisma.$queryRaw<any[]>`
+  let claimNode = await prisma.$queryRaw<any[]>`
     ${Prisma.raw(baseQuery)}
     WHERE c."id" = ${Number(claimId)}
     `;
 
-    claimNode = claimNode.map((claim) => {
-      return {
+  if (claimNode.length === 0) throw new Error("Claim not found");
+
+  let validations = await prisma.$queryRaw<any[]>`
+      ${Prisma.raw(baseQuery)}
+      WHERE n1."nodeUri" = ${claim_as_node_uri} AND c."id" != ${Number(claimId)}
+      ORDER BY c.id DESC
+      LIMIT ${limit}
+      OFFSET ${(page - 1) * limit}
+    `;
+
+  claimNode = claimNode.map((claim): GraphNode => {
+    return {
+      data: {
         id: `${claim.node_id}`,
         label: claim.label,
-
         raw: {
           claimId: `${claim.id}`,
           nodeId: `${claim.node_id}`,
           claim: claim.claim,
           page: 1,
         },
-      };
-    });
+      },
+    };
+  });
 
-    validations = validations.map((validation) => {
-      return {
+
+  validations = validations.map((validation): GraphNode => {
+    return {
+      data: {
         id: `${validation.node_id}`,
-        label: validation.sourceuri || validation.label,
+        label: getClaimNameFromNodeUri(validation.sourceuri) || validation.label,
         raw: {
           claimId: `${validation.id}`,
           nodeId: `${validation.node_id}`,
           claim: validation.claim,
           page: 0,
         },
-      };
-    });
+      },
+    };
+  });
 
-    const edges = validations.map((validation) => {
-      return {
-        id: `${validation.id}-${claimNode[0].id}`,
-        relation: validation.raw.claim,
-        target: claimNode[0].id,
-        source: `${validation.id}`,
+  const edges = validations.map((validation): GraphEdge => {
+    return {
+      data: {
+        id: `${validation.data.id}-${claimNode[0].data.id}`,
+        relation: validation.data.raw.claim,
+        target: claimNode[0].data.id,
+        source: `${validation.data.id}`,
         label: "validation",
         raw: {
-          endNodeId: `${validation.id}`,
-          startNodeId: `${claimNode[0].id}`,
-          startClaimId: `${claimNode[0].raw.claimId}`,
-          endClaimId: `${validation.raw.claimId}`,
+          endNodeId: `${validation.data.id}`,
+          startNodeId: `${claimNode[0].data.id}`,
+          startClaimId: `${claimNode[0].data.raw.claimId}`,
+          endClaimId: `${validation.data.raw.claimId}`,
         },
-      };
-    });
-
-    return {
-      nodes: [...claimNode, ...validations].map((claim) => {
-        return {
-          data: {
-            ...claim,
-          },
-        };
-      }),
-      edges: edges.map((edge) => {
-        return {
-          data: {
-            ...edge,
-          },
-        };
-      }),
+      }
     };
-  } catch (err) {
-    console.error(err);
-  }
+  });
+
+
+  return {
+    nodes: [...claimNode, ...validations],
+    edges
+  };
 };
 
 export const expandGraph = async (claimId: number, page: number, limit: number, host: string) => {
-  const graphResponse = await getGraphNode(claimId, page, limit, host);
-  if (!graphResponse) {
-    throw new Error("Failed to fetch graph nodes and edges");
-  }
-  const { nodes, edges } = graphResponse;
+  const { nodes, edges } = await getGraphNode(claimId, page, limit, host);
   return {
     nodes: nodes.slice(1),
     edges,
